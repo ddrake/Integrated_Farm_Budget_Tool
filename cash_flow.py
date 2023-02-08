7a72a7108107a94855bc60a6248462a3020b9aba
Dow Drake <dowdrake@gmail.com>
2023-02-08 14:49:22 -0800

    Add cash_flow module with tests.

--- cash_flow.py
+++ cash_flow.py
@@ -7,6 +7,10 @@ is to return total estimated cost for the farm for the given crop year
 corresponding to arbitrary sensitivity factors for price and yield.
 """
 from analysis import Analysis
+from cost import Cost
+from crop_ins import CropIns
+from gov_pmt import GovPmt
+from revenue import Revenue
 
 
 class CashFlow(Analysis):
@@ -25,3 +29,37 @@ class CashFlow(Analysis):
 
     def __init__(self, *args, **kwargs):
         super(CashFlow, self).__init__(*args, **kwargs)
+        self.cost = Cost(self.crop_year)
+        self.crop_ins = CropIns(self.crop_year)
+        self.gov_pmt = GovPmt(self.crop_year)
+        self.revenue = Revenue(self.crop_year)
+
+    def total_revenue(self, pf=1, yf=1):
+        """
+        GVBudget G37
+        """
+        return round(self.revenue.total_revenue_grain(pf, yf) +
+                     self.revenue.total_revenue_other() +
+                     self.gov_pmt.total_gov_pmt(pf, yf))
+
+    def total_non_land_operating_costs(self, pf=1, yf=1):
+        """
+        GVBudget G41
+        """
+        return round(self.cost.total_variable_cost(yf) -
+                     self.crop_ins.total_net_crop_ins_indemnity(pf, yf) +
+                     self.cost.total_overhead(yf))
+
+    def cash_flow_before_land_costs(self, pf=1, yf=1):
+        """
+        GVBudget G43
+        """
+        return (self.total_revenue(pf, yf) -
+                self.total_non_land_operating_costs(pf, yf))
+
+    def total_cash_flow(self, pf=1, yf=1):
+        """
+        GVBudget G52
+        """
+        return round(self.cash_flow_before_land_costs(pf, yf) -
+                     self.cost.total_land_expenses(yf))
