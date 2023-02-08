3a2d0fc17bfd9174d81289a1662cc960aba87afe
Dow Drake <dowdrake@gmail.com>
2023-02-08 14:52:42 -0800

    Correct test values based on changes made in 1/30 workbook, add tests for PLC

--- test_gov_pmt.py
+++ test_gov_pmt.py
@@ -4,21 +4,41 @@ from gov_pmt import GovPmt
 # but changes to program selections are OK.
 
 
-def test_total_gov_pmt():
+def test_total_gov_pmt_arco():
     g = GovPmt(2023)
 
     g.program_corn = GovPmt.ARC_CO
     g.program_soy = GovPmt.ARC_CO
     g.program_wheat = GovPmt.ARC_CO
 
-    total_gov_pmt = g.total_gov_pmt(1, 1)
+    total_gov_pmt = g.total_gov_pmt(pf=1, yf=1)
     assert total_gov_pmt == 0
 
-    total_gov_pmt = g.total_gov_pmt(0.6)
-    assert total_gov_pmt == 145655
+    total_gov_pmt = g.total_gov_pmt(pf=0.6, yf=1)
+    assert total_gov_pmt == 196452
 
-    total_gov_pmt = g.total_gov_pmt(yf=0.55)
-    assert total_gov_pmt == 358497
+    total_gov_pmt = g.total_gov_pmt(pf=1, yf=0.55)
+    assert total_gov_pmt == 375000
 
-    total_gov_pmt = g.total_gov_pmt(.75, .8)
-    assert total_gov_pmt == 83012
+    total_gov_pmt = g.total_gov_pmt(pf=.75, yf=.8)
+    assert total_gov_pmt == 135021
+
+
+def test_total_gov_pmt_plc():
+    g = GovPmt(2023)
+
+    g.program_corn = GovPmt.PLC
+    g.program_soy = GovPmt.PLC
+    g.program_wheat = GovPmt.PLC
+
+    total_gov_pmt = g.total_gov_pmt(pf=1, yf=1)
+    assert total_gov_pmt == 0
+
+    total_gov_pmt = g.total_gov_pmt(pf=0.6, yf=1)
+    assert total_gov_pmt == 375000
+
+    total_gov_pmt = g.total_gov_pmt(pf=1, yf=0.55)
+    assert total_gov_pmt == 0
+
+    total_gov_pmt = g.total_gov_pmt(pf=.75, yf=.8)
+    assert total_gov_pmt == 894
