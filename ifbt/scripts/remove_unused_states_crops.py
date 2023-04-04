13fba91908fea0743967b39b42a3629c9834a996
Dow Drake <dowdrake@gmail.com>
2023-04-03 22:04:35 -0700

    Fix several errors in script.

--- ifbt/scripts/remove_unused_states_crops.py
+++ ifbt/scripts/remove_unused_states_crops.py
@@ -1,34 +1,43 @@
 """
-Script remove_unused_states_crops
+Script remove_unused_states_crops_croptypes
 
 Remove items from combo premium data files which reference crops not in CROPS or
-states not in STATES
+states not in STATES or croptypes not in CROPTYPES
 
 Before running this script with new data, do the following steps:
 1. unprotect the corresponding spreadsheet tab
 2. move the tab to a new workbook
 3. export as tab-separated values
 4. delete the header row(s)
-5. for some tabs, additional processing may be needed.
+
+The files parameters.txt and counties.txt were processed using a text editor
 """
 from os import path
 from pathlib import Path
 import shutil
 
-
 STATES = set(
-    {'IL': '17', 'AL': '01', 'AR': '05', 'FL': '12', 'GA': '13', 'IN': '18',
+    {'IL': '17', 'AL': '1', 'AR': '5', 'FL': '12', 'GA': '13', 'IN': '18',
      'IA': '19', 'KS': '20', 'KY': '21', 'LA': '22', 'MD': '24', 'MI': '26',
      'MN': '27', 'MS': '28', 'MO': '29', 'NE': '31', 'NC': '37', 'ND': '38',
-     'OH': '39', 'PA': '41', 'SC': '45', 'SD': '46', 'TN': '47', 'VA': '51',
+     'OH': '39', 'PA': '42', 'SC': '45', 'SD': '46', 'TN': '47', 'VA': '51',
      'WV': '54', 'WI': '55', 'OK': '40', 'TX': '48'}.values())
 
 CROPS = set(
     {'Corn': '41', 'Soybeans': '81', 'Wheat': '11'}.values())
 
-FILES = ('options rates rateDiff prateDiff UnitFactor punitFactor enterpriseFactor ' +
-         'penterpriseFactor enterFactorRev pEnterFactorRev highRisk enterId betaid ' +
-         'eco grip griphr grp scoArp scoArpw scoYp').split()
+CROPTYPES = set({'Grain': '016', 'No crop specified': '997', 'Winter': '011'}.values())
+
+# These have the standard integer code or ccode in the first column
+FILES = ('options rates highRisk enterId betaid ' +
+         'eco grp scoArp scoArpw scoYp').split()
+
+# These have a one-digit risk level appended to the code with a decimal point
+RISK_FILES = ('rateDiff prateDiff UnitFactor punitFactor enterpriseFactor ' +
+              'penterpriseFactor enterFactorRev pEnterFactorRev').split()
+
+# These have a two-digit pvol value appended to the code with a decimal point
+REV_OPT_FILES = ('grip griphr').split()
 
 BETA_FILES = 'draws'.split()
 ENTER_FILES = 'discountEnter'.split()
@@ -39,11 +48,25 @@ OLDDIR = f'{DATADIR}/old'
 
 
 def filter_items_std(items):
-    return [it for it in items if it[0][:2] in STATES and it[0][5:7] in CROPS]
+    items = ([str(int(float(it[0])))] + it[1:] for it in items)
+    return [it for it in items if it[0][:-11] in STATES and it[0][-8:-6] in CROPS
+            and it[0][-6:-3] in CROPTYPES]
+
+
+def filter_items_risk(items):
+    items = ([f'{float(it[0]):.1f}'] + it[1:] for it in items)
+    return [it for it in items if it[0][:-13] in STATES and it[0][-10:-8] in CROPS
+            and it[0][-8:-5] in CROPTYPES]
+
+
+def filter_items_rev_opt(items):
+    items = ([f'{float(it[0]):.2f}'] + it[1:] for it in items)
+    return [it for it in items if it[0][:-14] in STATES and it[0][-11:-9] in CROPS
+            and it[0][-9:-6] in CROPTYPES]
 
 
 def filter_items_counties(items):
-    return [it for it in items if f'{int(it[0]):02d}' in STATES]
+    return [it for it in items if it[0] in STATES]
 
 
 def filter_items_beta(items):
@@ -80,6 +103,5 @@ def process_file_list(files, processor):
 
 
 process_file_list(FILES, filter_items_std)
-process_file_list(OTHER_FILES, filter_items_counties)
-process_file_list(BETA_FILES, filter_items_beta)
-process_file_list(ENTER_FILES, filter_items_enter)
+process_file_list(RISK_FILES, filter_items_risk)
+process_file_list(REV_OPT_FILES, filter_items_rev_opt)
