35ca2cbb5463eda3195aa58307d8510e5239be06
Dow Drake <dowdrake@gmail.com>
2023-04-04 12:32:49 -0700

    Update payment results for pf cases of ARC-CO

--- ifbt/tests/test_gov_pmt.py
+++ ifbt/tests/test_gov_pmt.py
@@ -13,13 +13,13 @@ def test_total_gov_pmt_arco():
     assert total_gov_pmt == 0
 
     total_gov_pmt = g.total_gov_pmt(pf=0.6, yf=1)
-    assert total_gov_pmt == 196452
+    assert total_gov_pmt == 198995
 
     total_gov_pmt = g.total_gov_pmt(pf=1, yf=0.55)
     assert total_gov_pmt == 375000
 
     total_gov_pmt = g.total_gov_pmt(pf=.75, yf=.8)
-    assert total_gov_pmt == 135021
+    assert total_gov_pmt == 137564
 
 
 def test_total_gov_pmt_plc():
