48802a7332ccb64dc54511040fb784023bc82739
Dow Drake <dowdrake@gmail.com>
2023-05-13 19:01:47 -0700

    Add a simple database-dependent test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import FarmYear
+from django.contrib.auth.models import User
+
+
+class FarmYearTestCase(TestCase):
+    def setUp(self):
+        joe = User.objects.create(username='joe124', password='verrysekrit')
+        self.farm_year = FarmYear.objects.create(user=joe, farm_name="Joey's farm",
+                                                 state_id=5, county_code=1)
+
+    def test_farm_year(self):
+        self.assertEqual(self.farm_year.farm_crops.count(), 2)
