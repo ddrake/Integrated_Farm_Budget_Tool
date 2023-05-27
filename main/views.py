eb01b28ca26a6de57face488d26f35691ff52662
Dow Drake <dowdrake@gmail.com>
2023-05-27 13:37:53 -0700

    Add dashboard; update home page.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import FarmYear
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def dashboard(request):
+    farm_years = FarmYear.objects.filter(user=request.user).all()
+    return render(request, 'main/dashboard.html', {'farm_years': farm_years})
