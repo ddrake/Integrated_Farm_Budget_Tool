20aca7531134a31f23979ab87df6c3a72e4bab9a
Dow Drake <dowdrake@gmail.com>
2023-07-04 08:12:21 -0700

    Add error logging

--- ifbt/settings.py
+++ ifbt/settings.py
@@ -130,4 +130,26 @@ IMPERSONATE = {
     'REQUIRE_SUPERUSER': True,
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / 'debug.log',
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file", "console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
 from .settings_local import *
