6c42630047ddb9fc4e533b5141863db65a2e0c78
Dow Drake <dowdrake@gmail.com>
2023-09-24 10:25:15 -0700

    Update for argument changes

--- core/models/test_gov_pmt.py
+++ core/models/test_gov_pmt.py
@@ -7,12 +7,10 @@ class GovPmtAllPLCMya4_80TestCase(TestCase):
     def setUp(self):
         # Somthing like Grandview corn.
         # For PLC, the only sensitivity is price sensitivity, which comes in via
-        # the sensitized MYA price
         self.govpmt = GovPmt(
-            crop_year=2023, state=17, county=119, crop_id=1, is_irr=False,
             plc_base_acres=4220, arcco_base_acres=0, plc_yield=160,
             estimated_county_yield=190, effective_ref_price=3.70,
-            natl_loan_rate=2.20, sens_mya_price=4.80)
+            natl_loan_rate=2.20, sens_mya_price=4.80, benchmark_revenue=801.09)
 
     def test_with_default_values(self):
         pmt = self.govpmt.prog_pmt_pre_sequest()
@@ -24,10 +22,9 @@ class GovPmtAllPLCMya3_80TestCase(TestCase):
     def setUp(self):
         # Somthing like Grandview corn.  Note sens_mya_price.
         self.govpmt = GovPmt(
-            crop_year=2023, state=17, county=119, crop_id=1, is_irr=False,
             plc_base_acres=4220, arcco_base_acres=0, plc_yield=160,
             estimated_county_yield=190, effective_ref_price=3.70,
-            natl_loan_rate=2.20, sens_mya_price=3.80)
+            natl_loan_rate=2.20, sens_mya_price=3.80, benchmark_revenue=801.09)
 
     def test_with_default_values(self):
         pmt = self.govpmt.prog_pmt_pre_sequest()
@@ -39,10 +36,9 @@ class GovPmtAllPLCMya3_20TestCase(TestCase):
     def setUp(self):
         # Somthing like Grandview corn.  Note sens_mya_price.
         self.govpmt = GovPmt(
-            crop_year=2023, state=17, county=119, crop_id=1, is_irr=False,
             plc_base_acres=4220, arcco_base_acres=0, plc_yield=160,
             estimated_county_yield=190, effective_ref_price=3.70,
-            natl_loan_rate=2.20, sens_mya_price=3.20)
+            natl_loan_rate=2.20, sens_mya_price=3.20, benchmark_revenue=801.09)
 
     def test_with_default_values(self):
         pmt = self.govpmt.prog_pmt_pre_sequest()
@@ -54,10 +50,9 @@ class GovPmtAllPLCMya2_80TestCase(TestCase):
     def setUp(self):
         # Somthing like Grandview corn.  Note sens_mya_price.
         self.govpmt = GovPmt(
-            crop_year=2023, state=17, county=119, crop_id=1, is_irr=False,
             plc_base_acres=4220, arcco_base_acres=0, plc_yield=160,
             estimated_county_yield=190, effective_ref_price=3.70,
-            natl_loan_rate=2.20, sens_mya_price=2.80)
+            natl_loan_rate=2.20, sens_mya_price=2.80, benchmark_revenue=801.09)
 
     def test_with_default_values(self):
         pmt = self.govpmt.prog_pmt_pre_sequest()
@@ -69,10 +64,9 @@ class GovPmtAllARCCOTestCase(TestCase):
     def setUp(self):
         # Somthing like Grandview corn
         self.govpmt = GovPmt(
-            crop_year=2023, state=17, county=119, crop_id=1, is_irr=False,
             plc_base_acres=0, arcco_base_acres=4220, plc_yield=160,
             estimated_county_yield=190, effective_ref_price=3.70,
-            natl_loan_rate=2.20, sens_mya_price=4.80)
+            natl_loan_rate=2.20, sens_mya_price=4.80, benchmark_revenue=801.09)
 
     def test_with_default_values(self):
         pmt = self.govpmt.prog_pmt_pre_sequest()
