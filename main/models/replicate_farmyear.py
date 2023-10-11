36fa707dfc3768a614eea9f55513fd2bcb491fe4
Dow Drake <dowdrake@gmail.com>
2023-10-11 14:07:02 -0700

    Move casts for more reliability in null cases

--- main/models/replicate_farmyear.py
+++ main/models/replicate_farmyear.py
@@ -86,7 +86,7 @@ insertedfsacrops (
     FROM insertedfarmyear
   ),
   n1.plc_base_acres, n1.arcco_base_acres, n1.plc_yield, n1.fsa_crop_type_id,
-  n1.effective_ref_price, n1.natl_loan_rate
+  n1.effective_ref_price::double precision, n1.natl_loan_rate::double precision
   FROM newfsacrops n1
   RETURNING farm_year_id, id as fsa_crop_id, fsa_crop_type_id
 ),
@@ -124,8 +124,10 @@ insertedcontracts (
   market_crop_id, contract_date, bushels, terminal, contract_number,
   delivery_start_date, delivery_end_date, basis_price, futures_price)
   SELECT
-  imc.market_crop_id, n3.contract_date, n3.bushels, n3.terminal, n3.contract_number,
-  n3.delivery_start_date, n3.delivery_end_date, n3.basis_price, n3.futures_price
+  imc.market_crop_id, n3.contract_date::date, n3.bushels, n3.terminal,
+  n3.contract_number, n3.delivery_start_date::date,
+  n3.delivery_end_date::date, n3.basis_price::double precision,
+  n3.futures_price::double precision
   FROM newcontracts n3 inner join insertedmarketcrops imc
   ON n3.market_crop_type_id = imc.market_crop_type_id
   RETURNING id
@@ -155,12 +157,13 @@ insertedfarmcrops (
   proj_price_disc_end)
   SELECT
   imc.market_crop_id, imc.farm_year_id, n4.planted_acres, n4.ta_aph_yield,
-  n4.adj_yield, n4.rate_yield, n4.ye_use, n4.ta_use, n4.subcounty, n4.coverage_type,
-  n4.product_type, n4.base_coverage_level, n4.sco_use,
-  n4.eco_level, n4.prot_factor, n4.ins_practice, n4.ins_practices,
-  n4.farm_crop_type_id, n4.ins_crop_type_id,
-  n4.cty_yield_final, n4.harv_price_disc_end,
-  n4.proj_price_disc_end
+  n4.adj_yield, n4.rate_yield, n4.ye_use, n4.ta_use, n4.subcounty,
+  n4.coverage_type::smallint, n4.product_type::smallint,
+  n4.base_coverage_level::double precision, n4.sco_use,
+  n4.eco_level::double precision, n4.prot_factor, n4.ins_practice,
+  n4.ins_practices::smallint[], n4.farm_crop_type_id, n4.ins_crop_type_id,
+  n4.cty_yield_final::date, n4.harv_price_disc_end::date,
+  n4.proj_price_disc_end::date
   FROM newfarmcrops n4 inner join insertedmarketcrops imc
   ON n4.market_crop_type_id = imc.market_crop_type_id
   RETURNING farm_year_id, id as farm_crop_id, farm_crop_type_id
@@ -262,38 +265,38 @@ select * FROM insertedfbcs LIMIT 1;
 def get_fy_dict(fy, user_id, live_to_loc):
     fy_dict = {
         'values': [
-            qt(fy.farm_name), nz(fy.county_code), nz(fy.crop_year),
-            nz(fy.cropland_acres_owned), nz(fy.variable_rented_acres),
-            nz(fy.cash_rented_acres), nz(fy.var_rent_cap_floor_frac),
-            nz(fy.annual_land_int_expense), nz(fy.annual_land_principal_pmt),
-            nz(fy.property_taxes), nz(fy.land_repairs),
-            nz(fy.eligible_persons_for_cap), nz(fy.state_id), nz(user_id),
-            bstr(fy.is_model_run_date_manual), nz(fy.other_nongrain_expense),
-            nz(fy.other_nongrain_income), dstr(fy.manual_model_run_date),
-            nz(fy.basis_increment), nz(fy.est_sequest_frac)
+            qt(fy.farm_name), fy.county_code, fy.crop_year,
+            fy.cropland_acres_owned, fy.variable_rented_acres,
+            fy.cash_rented_acres, fy.var_rent_cap_floor_frac,
+            fy.annual_land_int_expense, fy.annual_land_principal_pmt,
+            fy.property_taxes, fy.land_repairs,
+            fy.eligible_persons_for_cap, fy.state_id, user_id,
+            bstr(fy.is_model_run_date_manual), fy.other_nongrain_expense,
+            fy.other_nongrain_income, dstr(fy.manual_model_run_date),
+            fy.basis_increment, fy.est_sequest_frac
         ],
         'fsa_crops': []
     }
     for fa in fy.fsa_crops.all():
         fa_dict = {
             'values': [
-                nz(fa.plc_base_acres), nz(fa.arcco_base_acres), nz(fa.plc_yield),
-                nz(fa.fsa_crop_type_id), nz(fa.effective_ref_price),
+                fa.plc_base_acres, fa.arcco_base_acres, fa.plc_yield,
+                fa.fsa_crop_type_id, nz(fa.effective_ref_price),
                 nz(fa.natl_loan_rate)],
             'market_crops': []}
 
         for mc in fa.market_crops.all():
             mc_dict = {
                 'values': [
-                     nz(fa.fsa_crop_type_id), nz(mc.assumed_basis_for_new),
-                     nz(mc.market_crop_type_id), nz(mc.price_factor)],
+                     fa.fsa_crop_type_id, mc.assumed_basis_for_new,
+                     mc.market_crop_type_id, mc.price_factor],
                 'farm_crops': [],
                 'contracts': []}
 
             for ct in mc.contracts.all():
                 ct_dict = {
                     'values': [
-                        nz(mc.market_crop_type_id), dstr(ct.contract_date),
+                        mc.market_crop_type_id, dstr(ct.contract_date),
                         nz(ct.bushels), qt(ct.terminal), qt(ct.contract_number),
                         dstr(ct.delivery_start_date), dstr(ct.delivery_end_date),
                         nz(ct.basis_price), nz(ct.futures_price)]}
@@ -302,14 +305,14 @@ def get_fy_dict(fy, user_id, live_to_loc):
             for fc in mc.farm_crops.all():
                 fc_dict = {
                     'values': [
-                        nz(mc.market_crop_type_id), nz(fc.planted_acres),
-                        nz(fc.ta_aph_yield), nz(fc.adj_yield), nz(fc.rate_yield),
+                        mc.market_crop_type_id, fc.planted_acres,
+                        fc.ta_aph_yield, fc.adj_yield, fc.rate_yield,
                         bstr(fc.ye_use), bstr(fc.ta_use), qt(fc.subcounty),
                         nz(fc.coverage_type), nz(fc.product_type),
                         nz(fc.base_coverage_level), bstr(fc.sco_use),
-                        nz(fc.eco_level), nz(fc.prot_factor), nz(fc.ins_practice),
-                        astr(fc.ins_practices), nz(fc.farm_crop_type_id),
-                        nz(fc.ins_crop_type_id), dstr(fc.cty_yield_final),
+                        nz(fc.eco_level), fc.prot_factor, fc.ins_practice,
+                        astr(fc.ins_practices), fc.farm_crop_type_id,
+                        fc.ins_crop_type_id, dstr(fc.cty_yield_final),
                         dstr(fc.harv_price_disc_end), dstr(fc.proj_price_disc_end)],
                     'fbcs': []}
 
@@ -317,24 +320,24 @@ def get_fy_dict(fy, user_id, live_to_loc):
                     bc = fc.farmbudgetcrop
                     bc_dict = {
                         'values': [
-                            nz(bc.farm_yield),
-                            nz(bc.county_yield), nz(bc.yield_variability),
-                            nz(bc.other_gov_pmts), nz(bc.other_revenue),
-                            nz(bc.fertilizers), nz(bc.pesticides), nz(bc.seed),
-                            nz(bc.drying), nz(bc.storage), nz(bc.other_direct_costs),
-                            nz(bc.machine_hire_lease), nz(bc.utilities),
-                            nz(bc.machine_repair), nz(bc.fuel_and_oil),
-                            nz(bc.light_vehicle), nz(bc.machine_depr),
-                            nz(bc.labor_and_mgmt), nz(bc.building_repair_and_rent),
-                            nz(bc.building_depr), nz(bc.insurance),
-                            nz(bc.misc_overhead_costs), nz(bc.interest_nonland),
-                            nz(bc.other_overhead_costs), nz(bc.rented_land_costs),
+                            bc.farm_yield,
+                            bc.county_yield, bc.yield_variability,
+                            bc.other_gov_pmts, bc.other_revenue,
+                            bc.fertilizers, bc.pesticides, bc.seed,
+                            bc.drying, bc.storage, bc.other_direct_costs,
+                            bc.machine_hire_lease, bc.utilities,
+                            bc.machine_repair, bc.fuel_and_oil,
+                            bc.light_vehicle, bc.machine_depr,
+                            bc.labor_and_mgmt, bc.building_repair_and_rent,
+                            bc.building_depr, bc.insurance,
+                            bc.misc_overhead_costs, bc.interest_nonland,
+                            bc.other_overhead_costs, bc.rented_land_costs,
                             nz(bc.budget_id, offset=(3 if live_to_loc else 0)),
-                            qt(bc.description), nz(bc.state_id),
+                            qt(bc.description), bc.state_id,
                             nz(bc.farm_crop_type_id), bstr(bc.is_irr), bstr(bc.is_rot),
                             nz(bc.budget_crop_id, offset=(63 if live_to_loc else 0)),
-                            dstr(bc.budget_date), nz(bc.baseline_yield_for_var_rent),
-                            bstr(bc.is_farm_yield_final), nz(bc.yield_factor),
+                            dstr(bc.budget_date), bc.baseline_yield_for_var_rent,
+                            bstr(bc.is_farm_yield_final), bc.yield_factor,
                             bstr(bc.are_costs_final)]}
                     fc_dict['fbcs'].append(bc_dict)
                 mc_dict['farm_crops'].append(fc_dict)
