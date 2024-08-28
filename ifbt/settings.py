ce7d495f569debaed0ebf6c71cfcb88bc0ca9a44
Dow Drake <dowdrake@gmail.com>
2024-08-27 19:56:21 -0700

    Remove duplicate definition of ALLOWED_HOSTS (overridden in settings_local)

--- ifbt/settings.py
+++ ifbt/settings.py
@@ -16,8 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
