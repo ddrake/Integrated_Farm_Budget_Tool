50a697a27fc3b54646098c7cf7b58072c969ab48
Dow Drake <dowdrake@gmail.com>
2024-08-27 19:55:12 -0700

    Login url should not be defined here, it's included with the contrib.auth urls.

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
 from .views import (RegistrationView, ActivateView, CheckEmailView,
-                    login, DeleteAccountView)
+                    DeleteAccountView)
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', ActivateView.as_view(), name='activate'),
     path('check-email/', CheckEmailView.as_view(), name="check_email"),
-    path('login/', login, name='login'),
     path('delete-account/', DeleteAccountView.as_view(), name='delete_account'),
 ]
