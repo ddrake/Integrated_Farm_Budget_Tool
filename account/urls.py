128488811c57a339a8b3ffb47e6f92888817a7a8
Dow Drake <dowdrake@gmail.com>
2025-05-29 14:07:48 -0700

    Remove registration feature temporarily

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from .views import (RegistrationView, ActivateView, CheckEmailView,
                     DeleteAccountView)
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='register'),
+    # path('register/', RegistrationView.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', ActivateView.as_view(), name='activate'),
     path('check-email/', CheckEmailView.as_view(), name="check_email"),
     path('delete-account/', DeleteAccountView.as_view(), name='delete_account'),
